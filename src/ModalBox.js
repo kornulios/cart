1c1d0c53d297699f63c8ce8e4fa65e16c2e4fc65
kornulios <andriy.iatsyn@gmail.com>
2017-07-10 18:29:34 +0200

    modal box improved

--- src/ModalBox.js
+++ src/ModalBox.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
 
 class ModalBox extends Component {
+
+  submitForm() {
+    let val = 'Kalunga';
+    this.props.onSubmit(val);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    if (e.target.id === 'modal-id') this.props.onCancel();
+  }
+
   render() {
+
+    const modalFooter = (
+      <div className='modal-footer'>
+        <button onClick={this.submitForm.bind(this)}>Submit</button>
+        <button onClick={this.props.onCancel}>Cancel</button>
+      </div>
+    );
+
     return (
-      <div className='modal-box'>
+      <div className='modal-box' id='modal-id' onClick={this.handleClick.bind(this)}>
         <div className='modal-content'>
-          <p>{this.props.text}</p>
-          <input type="text" value="Balue"></input>
-          <br />
-          <button onClick={this.props.onSubmit}>Submit</button>
-          <button>Cancel</button>
+          <div className='modal-body'>
+            <p>{this.props.text}</p>
+            <input type="text" value="Balue"></input>
+          </div>
+          {modalFooter}
         </div>
       </div>
     );
